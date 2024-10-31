cfbc4a70b47ff6cd53397be58c94372cb701b9c2
kanyijohn <mrkanyi8@gmail.com>
2024-10-31 13:23:13 +0300

    input dictionary done

--- app/main.py
+++ app/main.py
@@ -83,7 +83,6 @@ def main():
   
 input_data = add_sidebar()
 st.write(input_data)
-
 with st.container():
     st.title("Breast Cancer Predictor")
     st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
